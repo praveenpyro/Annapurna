db0fbc01380ceaca9396ef6c7443ebf05192fd2f
RISHANK\praveen <juluri.prvn@gmail.com>
2023-05-21 10:42:00 -0400

    Added api utility

---
+++ src/api/callApi.js
@@ -0,0 +1,30 @@
+// callApi.js
+import axios from 'axios';
+
+// Create an Axios instance
+const apiInstance = axios.create();
+
+// Add an interceptor to send JWT token with each request
+apiInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('jwtToken'); // Retrieve the JWT token from wherever it's stored
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+// Utility function to make API calls
+export async function callApi(url, method = 'GET', data = null, options = {}) {
+  try {
+    const response = await apiInstance({
+      url,
+      method,
+      data,
+      ...options,
+    });
+    return response.data;
+  } catch (error) {
+    console.error('API Error:', error);
+    throw error;
+  }
+}
